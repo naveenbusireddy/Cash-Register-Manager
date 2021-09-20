64226f79635cff9e3fd57743d3452abc8ce9b8ad
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-20 19:45:15 +0530

    Feature update

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message ");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
     hideMessage();
@@ -9,18 +11,31 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
         if(cashGiven.value >= billAmount.value) {
             const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
+
         } else {
             showMessage("Cash given should be greater than or equal to bill amount ");
         }
 
     } else {
-        showMessage("Bill amount will greater than 0");
+        showMessage("Bill amount will be greater than 0");
     }
 });
+
+function calculateChange(amountToBeReturned) {
+    for( let i=0; i<availableNotes.length; i=i+1)
+    {
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
 
 function hideMessage() {
     message.style.display = "none";
 }
+
 function showMessage(msg) {
     message.style.display = "block";
     message.innerText = msg;
