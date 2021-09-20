c4f2ac93999c53dee9379f08be65d2c8177cce31
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-20 23:11:09 +0530

    fix: input type number

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
     hideMessage();
     if(billAmount.value > 0) {
+
         if(cashGiven.value >= billAmount.value) {
+
             const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
 
@@ -22,7 +24,8 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
 });
 
 function calculateChange(amountToBeReturned) {
-    for( let i=0; i<availableNotes.length; i=i+1)
+
+    for( let i=0; i<availableNotes.length; i++)
     {
         const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
 
