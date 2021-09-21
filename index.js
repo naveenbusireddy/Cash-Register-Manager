35830d363a26d6be5aacad7981d4eab9256275fd
naveenbusireddy <naveenbusireddy@gmail.com>
2021-09-21 11:47:32 +0530

    fix: input data type and variable

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
     hideMessage();
-    if(billAmount.value > 0) {
+    if (Number(billAmount.value) > 0) {
+        if (Number(cashGiven.value) >= Number(billAmount.value)) {
 
-        if(cashGiven.value >= billAmount.value) {
-
-            const amountToBeReturned = cashGiven.value - billAmount.value;
+            const amountToBeReturned = Number(cashGiven.value) - Number(billAmount.value);
             calculateChange(amountToBeReturned);
 
         } else {
@@ -25,8 +24,7 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
 
 function calculateChange(amountToBeReturned) {
 
-    for( let i=0; i<availableNotes.length; i++)
-    {
+    for (var i = 0; i < availableNotes.length; i++) {
         const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
 
         amountToBeReturned = amountToBeReturned % availableNotes[i];
